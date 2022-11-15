3e497e099c0abf768f7bb78274613fb90846dbe3
Amy <wilford.amy@gmail.com>
2022-11-15 14:58:30 -0500

    updated deployment path

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import {Switch, Route, useRouteMatch} from 'react-router-dom';
-import './App.css';
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import "./App.css";
 
 // import components
-import Header from './components/Header';
-import Pokemon from './pages/Pokemon';
-import PokemonDetails from './pages/PokemonDetails';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Pokemon from "./pages/Pokemon";
+import PokemonDetails from "./pages/PokemonDetails";
+import Footer from "./components/Footer";
 
-
-function App() {  
-  const match=useRouteMatch();
+function App() {
+  const match = useRouteMatch();
 
   return (
     <div className="App">
-    <div className = 'container'>
-      <Header />
+      <div className="container">
+        <Header />
+      </div>
+      <Switch>
+        <Route exact path={`${match.url}pokemon/:id`}>
+          <PokemonDetails />
+        </Route>
+        <Route exact path="/Mistys-Pokedex">
+          <Pokemon />
+        </Route>
+      </Switch>
+      <div className="container">
+        <Footer />
+      </div>
     </div>
-    <Switch>
-      <Route exact path={`${match.url}pokemon/:id`}>
-        <PokemonDetails />
-      </Route>
-      <Route exact path = '/'>
-        <Pokemon />
-      </Route>
-    </Switch>
-    <div className ="container">
-      <Footer />
-    </div>
-    </div>
-   
   );
 }
 
